600db1c409b8364a91c3e8bba39c2395e291e26a
Dev-Guy <devguy@devguy.com>
2025-06-07 14:54:01 -0700

    From ash-hq (#1)

---
+++ lib/geo_web/controllers/page_html.ex
@@ -0,0 +1,10 @@
+defmodule GeoWeb.PageHTML do
+  @moduledoc """
+  This module contains pages rendered by PageController.
+
+  See the `page_html` directory for all templates available.
+  """
+  use GeoWeb, :html
+
+  embed_templates "page_html/*"
+end
