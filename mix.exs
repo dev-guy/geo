fb89e467d0637264323cf0351eb02f227b37635d
Dev-Guy <devguy@devguy.com>
2025-06-29 07:21:29 -0700

    Add tests for Collapse + up arrow scenario (#62)

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Geo.MixProject do
     base_config = [
       app: :geo,
       # GEO_VERSION
-      version: "0.3.0",
+      version: "0.3.1",
       # See also .tool-versions
       # Mishka Chelekom requires 1.17
       elixir: "~> 1.18",
